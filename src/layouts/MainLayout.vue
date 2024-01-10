5adbd306ff3fb8d3698448524e297153211537ce
Nate Goodman <nate.goodman@sparkbusinessworks.com>
2024-01-10 16:42:44 -0500

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,88 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-red text-white">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> K & G Tool Co </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Navigate </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-red text-white">
+      <q-toolbar class="q-pa-md">
+        <div class="footer">
+          © 2024 K & G Tool Co. All Rights Reserved. Contact Us: 16679 Comstock
+          Grand Haven, Michigan 49417. Phone: 616-846-3690 Fax: 616-846-5935
+        </div>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Home",
+  },
+  {
+    title: "Machines/Automation",
+  },
+  {
+    title: "Machining",
+  },
+  {
+    title: "Resources",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
+<style>
+.footer {
+  text-align: center;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-page class="q-pa-md flex flex-center">
+    <div>
+      <img
+        alt="KGTool"
+        src="~assets/Logo.png"
+        style="width: 200px; height: 200px"
+      />
+    </div>
+    <div class="text-center">
+      <h1 class="h1">
+        Designer and manufacturer of equipment for today's automotive, food and
+        pharmaceutical industries
+      </h1>
+      <p>
+        In today's fast-paced world, there are two things companies usually seem
+        to have too little of...
+        <span style="font-weight: bold">time and money</span>. K & G Tool has
+        the unique ability to help you save both!
+      </p>
+    </div>
+    <div>
+      <img
+        src="~assets/k-g_building.jpg"
+        alt="KGBuilding"
+        style="width: 640px; height: 480px"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
+
+<style>
+.h1 {
+  font-size: 32px;
+  margin-bottom: 16px;
+}
+
+p {
+  font-size: 18px;
+  line-height: 1.6;
+}
+</style>
