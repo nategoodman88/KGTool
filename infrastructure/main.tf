5b15f69a730e7b5c983c3973084137c54daec919
Nate Goodman <nategoodman88@gmail.com>
2026-04-20 16:48:30 -0400

    Change S3 bucket for Terraform backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "kgtool-terraform-state-prod"
+    bucket         = "tf-states-730335427928-us-east-2-an"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-2"
     dynamodb_table = "tofu-state-locks"
