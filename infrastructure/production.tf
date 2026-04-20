004131fb7737d44c03ae7dfc398370a62ebf4c95
Nate Goodman <nategoodman88@gmail.com>
2026-04-20 17:04:00 -0400

    Remove unused S3 bucket resources
    Removed S3 bucket resources for terraform state management.

--- infrastructure/production.tf
+++ infrastructure/production.tf
@@ -1,28 +1,3 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tf-states-730335427928-us-east-2-an"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
 resource "aws_s3_bucket" "kandgtoolco_com" {
   bucket = "kandgtoolco.com"
 }
