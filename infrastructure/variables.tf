219822c2e53cb71785b397a661b3fadcb733c079
Nate Goodman <nategoodman88@gmail.com>
2026-04-20 16:39:09 -0400

    Update aws_state_locks_dynamodb_table variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,19 @@
 variable "aws_region" {
+  type    = string
   default = "us-east-2"
 }
 
 variable "aws_s3_terraform_state_bucket" {
+  type    = string
   default = "kgtool-terraform-state-prod"
 }
 
 variable "aws_s3_terraform_state_bucket_key" {
+  type    = string
   default = "global/s3/terraform.tfstate"
 }
 
 variable "aws_state_locks_dynamodb_table" {
-  default = "terraform-state-locks"
+  type    = string
+  default = "tofu-state-locks"
 }
